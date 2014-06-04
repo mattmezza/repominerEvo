cfcd26d6ec4dc7ff61dd17b1fc09ffa3c2eb2c81
intersimone999 <intersimone999@hotmail.it>
2014-06-04 17:41:39 +0200

    Progetti 17-32 completati

---
+++ sql/Tests/Progetto31.sql
@@ -0,0 +1,21 @@
+USE repominer;
+
+-- Crea il progetto
+INSERT INTO `projects` (`id`, `name`, `versioning_url`) VALUES (1, 'Progetto 31', 'https://github.com/mattmezza/repominerEvo.git');
+
+-- Crea il packaged con nome vuoto
+INSERT INTO `import` (`id`, `name`) VALUES (1, '');
+INSERT INTO `source_containers` (`id`, `project`, `import_id`) VALUES (1, 1, 1);
+
+-- Crea la classe
+INSERT INTO `types` (`id`, `lines_number`, `src_file_location`, `source_container`) VALUES (1, 0, 'Primo.java', 1);
+
+-- Crea la lista di cambiamenti
+INSERT INTO `changes` 	(`id`, `hash`, `commit_date`, `dev_mail`, `dev_id`, `message`, `project`) VALUES 
+						(1, 'd54d3472148d60d0f9685b80144904b8a72e4dc6', '2013-08-13', 'a@b.it', 'Alfa', '', 1);
+INSERT INTO `changes` 	(`id`, `hash`, `commit_date`, `dev_mail`, `dev_id`, `message`, `project`) VALUES 
+						(2, 'd54d3472148d60d0f9685b80144904b8a72e4dc6', '2013-08-23', 'a@b.it', 'Alfa', '', 1);
+INSERT INTO `changes_for_commit` 	(`id`, `change_hash`, `modified_file`, `insertions`, `deletions`) VALUES 
+									(1, 1, 'Erased.pdf', 0, 0);
+INSERT INTO `changes_for_commit` 	(`id`, `change_hash`, `modified_file`, `insertions`, `deletions`) VALUES 
+									(2, 2, 'Erased2.pdf', 0, 0);
