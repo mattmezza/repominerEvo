d00faac8009c5b154e41e1dd1090c6122b976d4b
Matteo Merola <mattmezza@gmail.com>
2014-07-03 16:45:53 +0200

    Mega merge minor fix

--- src/it/unisa/sesa/repominer/metrics/ProjectMetrics.java
+++ src/it/unisa/sesa/repominer/metrics/ProjectMetrics.java
@@ -22,7 +22,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
