e44de3385e7dfc7e2a4c76c0bb48845349e1d00a
Matteo Merola <mattmezza@gmail.com>
2014-07-09 11:36:09 +0200

    Minor change to a string

--- src/it/unisa/sesa/repominer/preferences/exceptions/PeriodLengthTooLong.java
+++ src/it/unisa/sesa/repominer/preferences/exceptions/PeriodLengthTooLong.java
@@ -3,7 +3,7 @@ package it.unisa.sesa.repominer.preferences.exceptions;
 public class PeriodLengthTooLong extends Exception {
 
 	public PeriodLengthTooLong(int pLimit, int pLength) {
-		super("Specified period length is too long! (limit="+pLimit+" length="+pLength);
+		super("Specified period length is too long! (limit="+pLimit+" length="+pLength+")");
 	}
 	
 }
